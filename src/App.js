097f4415bd5cf612a0dd9c35d141b1a825d86569
Nilan Saha <nilansaha@gmail.com>
2021-03-10 19:36:19 -0800

    Removed test checked and pushed legit stuff

--- src/App.js
+++ src/App.js
@@ -9,14 +9,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <RogueElement>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
         </a>
       </header>
     </div>
